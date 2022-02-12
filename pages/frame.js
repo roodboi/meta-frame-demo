5b2cb3a9b16ce995ed4aaa6a4975e72737786e5c
roodboi <dimitrikennedy@gmail.com>
2022-02-12 13:24:27 -0500

    1 file modified

--- pages/frame.js
+++ pages/frame.js
@@ -1,18 +1,17 @@
 import { useEffect } from 'react';
 import Head from 'next/head';
-import { initializeMetaFrame } from '@plugins.chat/drift-meta-frame'
+import { initializeMetaFrame } from '@plugins.chat/drift-meta-frame';
 
 export default function Frame() {
   useEffect(() => {
-    initializeMetaFrame({ embed_id: '65477u96atme' })
-  })
+    initializeMetaFrame({ embed_id: '65477u96atme' });
+  });
 
   return (
     <>
       <Head>
         <title>Meta Frame demo | the frame</title>
       </Head>
-      asdf
     </>
   );
 }
